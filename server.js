ad30a28f1d615e95297963568a701443ad096d59
anupama4you <43500022+anupama4you@users.noreply.github.com>
2021-11-17 04:01:14 +0530

    updated the port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser')
 const cors = require('cors');
 
 const app = express()
-const port = 3000
+const port = 4000
 
 // parse requests of content-type: application/json
 app.use(bodyParser.json());
